62b31034a76beba66a8278714906b285ddc8272b
nipplelion <piolotorrico@yahoo.com>
2023-03-25 10:10:33 -0700

    Add random dogs and cats together

--- app/src/main/java/com/bignerdranch/android/randompet/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/randompet/MainActivity.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     var petImageURL = ""
@@ -30,7 +31,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun getNextImage(button: Button, imageView: ImageView) {
         button.setOnClickListener {
-            getDogImageURL()
+            var choice = Random.nextInt(2)
+            if (choice == 0) {
+                getCatImageURL()
+            } else {
+                getDogImageURL()
+            }
+
 
             Glide.with(this)
                 . load(petImageURL)
@@ -46,6 +53,27 @@ class MainActivity : AppCompatActivity() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
                 Log.d("Dog", "response successful$json")
                 petImageURL = json.jsonObject.getString("message")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
+    }
+
+    private fun getCatImageURL() {
+        val client = AsyncHttpClient()
+
+        client["https://api.thecatapi.com/v1/images/search", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful$json")
+                var resultsJSON = json.jsonArray.getJSONObject(0)
+                petImageURL = resultsJSON.getString("url")
             }
 
             override fun onFailure(
